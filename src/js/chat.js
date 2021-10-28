135e4da41577cf771354f19917af340a2a6be94f
Yanessa <yanessa_n@mail.ru>
2021-10-28 19:06:49 +0300

    Add deleting a messages

--- src/js/chat.js
+++ src/js/chat.js
@@ -2,6 +2,7 @@ const form = document.querySelector('.form');
 const input = document.querySelector('.form_input');
 const formButton = document.querySelector('.form_button');
 const dialogPage = document.querySelector('.dialog_page');
+let idMessage = 0;
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -10,10 +11,17 @@ form.addEventListener('submit', (event) => {
     return
   } 
   dialogPage.innerHTML += `
-    <div class="dialog_page-message">
-      <p class="dialog_page-message-text">${inputText}</p>
+    <div class="dialog_page-message" id="${idMessage++}">
+      <p class="dialog_page-message-text-p">${inputText}</p>
+      <button class="dialog_page-message-button delete" id="delete-${idMessage++}">-</button>
     </div>
   `
   input.value = '';
+  const buttonsDelete = document.querySelectorAll('.delete');
+  buttonsDelete.forEach(function(buttonDelete){
+    buttonDelete.addEventListener('click', (event) => {
+      event.target.parentNode.remove();
+    })
+  });
 });
 
